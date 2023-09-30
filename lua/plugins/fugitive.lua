e4b36ae5dad9949b4d6da27d8c7711608b58687d
Albhieri <alcrivico@gmail.com>
2023-09-30 11:49:06 -0600

    Keymaps y cmd sueron agregados a Fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
 	"tpope/vim-fugitive",
+	cmd = {"G", "Git"},
+	keys = {
+		{"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git Fetch"},
+		{"<leader>gl", ":Git pull<cr>", desc = "Git Pull"},
+	},
 }
