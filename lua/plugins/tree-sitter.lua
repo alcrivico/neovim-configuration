12e998e23c688b692e0e186716fe2bcbb81c2718
Albhieri <alcrivico@gmail.com>
2023-09-30 16:25:51 -0600

    LSP, Mason, Tree-Sitter, Surround y Lua agregados

---
+++ lua/plugins/tree-sitter.lua
@@ -0,0 +1,47 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	build = ":TSUpdate",
+	event = "VeryLazy",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"luadoc",
+			"c",
+			"cpp",
+			"html",
+			"css",
+			"javascript",
+			"json",
+			"java",
+			"kotlin",
+			"commonlisp",
+			"python",
+			"c_sharp",
+			"regex",
+		},
+		highlight = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@conditional.outer",
+					["ic"] = "@conditional.inner",
+					["al"] = "@loop.outer",
+					["il"] = "@loop.inner",
+				}
+			}
+		}
+	}
+}
