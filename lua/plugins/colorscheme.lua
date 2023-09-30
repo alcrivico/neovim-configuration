16ae2de42f45a0276f9914bd389a374c83fd04c8
Albhieri <alcrivico@gmail.com>
2023-09-30 12:37:19 -0600

    GitSigns fue agregado y se cambio a Ayu-Mirage

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,6 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme ayu]])
-		end,
+		vim.cmd([[colorscheme ayu-mirage]])
+	end,
 }
