70b860b94e0e207079766f08cf08ac20d1e7103f
Albhieri <alcrivico@gmail.com>
2023-10-04 20:28:26 -0600

    Se modificó la configuració de CMP

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,6 @@
+return {
+  "L3MON4D3/LuaSnip",
+  version = "v2.*",
+  main = "alcrivico.plugins.luasnip",
+  config = true,
+}
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,55 +1,16 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    "onsails/lspkind.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-git",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
+    "windwp/nvim-autopairs",
   },
-  config = function ()
-    local cmp = require("cmp")
-
-    local luasnip = require("luasnip")
-
-    local lspkind = require("lspkind")
-
-    -- load vs-code like snippets from plugins (e.g. frinedly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    vim.opt.completeopt = "menu,menuone,noselect"
-    cmp.setup({
-      snippet = {
-        expand = function ()
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      -- MAPPING KEYS
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggest
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggest
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      }),
-      -- SOURCES FOR AUTOCOMPLETION
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" }, -- LSP
-        { name = "luasnip" }, -- Snippets
-        { name = "buffer" }, -- Text within current buffer
-        { name = "path" }, -- File system paths
-      }),
-      -- CONFIGURE LSPKIND FOR VS-CODE LIKE ICONS
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
-      },
-    })
-  end,
+  event = "VeryLazy",
+  main = "alcrivico.plugins.nvim-cmp",
+  config = true,
 }
